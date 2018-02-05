1d992649d6c046ebe494012b46f9d638e96e2d9f
joelee2017 <as955133@gmail.com>
2018-02-05 12:47:07 +0800

    設定欄位顯示格式與中文欄位標題(一&二)

---
+++ WebAppTest/Metadatas/Products.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTest.Models
+{
+    [MetadataType(typeof(ProductsMetadata))]
+    public partial class Products
+    {
+       
+    }
+}
---
+++ WebAppTest/Metadatas/ProductsMetadata.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppTest.Models
+{
+    public class ProductsMetadata
+    {
+        [StringLength(40,ErrorMessage = "{0}最長{1}")]
+        public string ProductName { get; set; }
+    }
+}
