8fa934d1a0947d5227fc046556ecd79bc311b5f6
joelee2017 <as955133@gmail.com>
2018-02-09 15:39:58 +0800

    利用IValidatableObject介面實作自訂驗証

--- WebAppTest/Metadatas/OrderMetadata.cs
+++ WebAppTest/Metadatas/OrderMetadata.cs
@@ -7,7 +7,14 @@ namespace WebAppTest.Models
     {
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
-
         public Nullable<System.DateTime> OrderDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> RequiredDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> ShippedDate { get; set; }
     }
 }
--- WebAppTest/Metadatas/Orders.cs
+++ WebAppTest/Metadatas/Orders.cs
@@ -7,7 +7,15 @@ using System.Web;
 namespace WebAppTest.Models
 {
     [MetadataType(typeof(OrderMetadata))]
-    public partial class Orders
+    public partial class Orders : IValidatableObject//利用IValidatableObject介面實作自訂驗証
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+           if(RequiredDate > OrderDate.Value.AddMonths(1))
+            {
+               yield return new ValidationResult(
+                   "訂購商品必須在1個月內取貨", new string[] { "RequiredDate", "OrderDate" });
+            }
+        }
     }
 }
